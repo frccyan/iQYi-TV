71f9c95342398224a988e60d6ad01ea402af26c2
RY <e5277@outlook.com>
2026-01-11 22:46:19 +0800

    Change default settings for auto skip and next episode

--- src/components/SkipController.tsx
+++ src/components/SkipController.tsx
@@ -68,8 +68,8 @@ export default function SkipController({
     const loadUserSettings = () => {
       const savedEnableAutoSkip = localStorage.getItem('enableAutoSkip');
       const savedEnableAutoNextEpisode = localStorage.getItem('enableAutoNextEpisode');
-      const userAutoSkip = savedEnableAutoSkip !== null ? JSON.parse(savedEnableAutoSkip) : true;
-      const userAutoNextEpisode = savedEnableAutoNextEpisode !== null ? JSON.parse(savedEnableAutoNextEpisode) : true;
+      const userAutoSkip = savedEnableAutoSkip !== null ? JSON.parse(savedEnableAutoSkip) : false;
+      const userAutoNextEpisode = savedEnableAutoNextEpisode !== null ? JSON.parse(savedEnableAutoNextEpisode) : false;
 
       setBatchSettings(prev => ({
         ...prev,
